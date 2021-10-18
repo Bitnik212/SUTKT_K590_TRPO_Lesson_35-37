d15a313676669ae49b0260c489cdac3719083381
Николай Никоноров <nnv@nikolay.moe>
2021-10-18 14:29:34 +0300

    done/WebRequest

--- WebRequest/MainWindow.xaml.cs
+++ WebRequest/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿namespace WebRequest
+﻿using System.IO;
+using System.Linq;
+using System.Net;
+using System.Windows;
+
+namespace WebRequest
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -9,5 +14,48 @@
         {
             InitializeComponent();
         }
+        private void request_Click(object sender, RoutedEventArgs e)
+        {
+            // Создать объект запроса
+            var request = System.Net.WebRequest.Create(txb_url.Text);
+
+            // Получить ответ с сервера
+            WebResponse response = request.GetResponse();
+
+            // Получаем поток данных из ответа
+            using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+            {
+                // Выводим исходный код страницы
+                string line;
+                while ((line = stream.ReadLine()) != null)
+                    txb_sourceCode.Text += line + "\n";
+            }
+
+            // Получаем некоторые данные о сервере
+            string messageServer = "Целевой URL: \t" + request.RequestUri + "\nМетод запроса: \t" + request.Method +
+                                   "\nТип полученных данных: \t" + response.ContentType + "\nДлина ответа: \t" + response.ContentLength + "\nЗаголовки";
+
+            // Получаем заголовки, используем LINQ
+            WebHeaderCollection whc = response.Headers;
+            var headers = Enumerable.Range(0, whc.Count)
+                .Select(p =>
+                {
+                    return new
+                    {
+                        Key = whc.GetKey(p),
+                        Names = whc.GetValues(p)
+                    };
+                });
+
+            foreach (var item in headers)
+            {
+                messageServer += "\n  " + item.Key + ":";
+                foreach (var n in item.Names)
+                    messageServer += "\t" + n;
+            }
+
+            txb_serverInfo.Text = messageServer;
+        }
+
     }
 }
