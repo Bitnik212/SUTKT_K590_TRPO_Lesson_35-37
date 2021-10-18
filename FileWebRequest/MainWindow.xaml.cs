60d34e3687f8c9e0eb53fe9846fbfca72e018f53
Николай Никоноров <nnv@nikolay.moe>
2021-10-18 14:36:57 +0300

    done/FileWebRequest

--- FileWebRequest/MainWindow.xaml.cs
+++ FileWebRequest/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace FileWebRequest
+﻿using System.IO;
+using System.Net;
+using System.Windows;
+
+namespace FileWebRequest
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -9,5 +13,29 @@
         {
             InitializeComponent();
         }
+        
+        private void openFile_Click(object sender, RoutedEventArgs e)
+        {
+            string filename = txb_fileuri.Text;
+            System.Net.FileWebRequest request = 
+                (System.Net.FileWebRequest)WebRequest.Create(filename);
+
+            using (StreamReader sr = new StreamReader(request.GetResponse().GetResponseStream()))
+            {
+                txb_fileContent.Text = sr.ReadToEnd();
+            }
+        }
+        
+        private void writeFile_Click(object sender, RoutedEventArgs e)
+        {
+            WebRequest request = WebRequest.Create(txb_fileuri.Text);
+            request.Method = "PUT";
+            using (StreamWriter sw = new StreamWriter(request.GetRequestStream()))
+            {
+                sw.Write(txb_writefile.Text);
+            }
+        }
+
+
     }
 }
