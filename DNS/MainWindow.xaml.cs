7edc0b9f77287695bf1775bee527ce0c4f7fedbf
Николай Никоноров <nnv@nikolay.moe>
2021-10-18 14:38:50 +0300

    done/DNS

--- DNS/MainWindow.xaml.cs
+++ DNS/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace DNS
+﻿using System.Net;
+using System.Windows;
+
+namespace DNS
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -8,6 +11,19 @@
         public MainWindow()
         {
             InitializeComponent();
+            
+            string hostname = "www.google.com", message = "IP адреса для домена " + hostname + "\n";
+            IPHostEntry entry = Dns.GetHostEntry(hostname);
+
+            foreach (IPAddress a in entry.AddressList)
+                message += "  --> "+ a.ToString() + "\n";
+
+            message += "\nАльтернативное имя домена: ";
+            foreach (string aliasName in entry.Aliases)
+                message += aliasName + "\n";
+
+            message += "\nРеальное название хоста: " + entry.HostName;
+            MessageBox.Show(message);
         }
     }
 }
